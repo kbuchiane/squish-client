df0b5c0865d124bfc898348aee6a9e38a3f928cd
kbuchiane <kbuchiane@gmail.com>
2020-04-17 16:11:42 -0400

    client working

--- src/mixins/main.js
+++ src/mixins/main.js
@@ -8,4 +8,4 @@ app.get('/', function(request, response) {
 }).listen(8081);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Client running at http://127.0.0.1:8081/');
