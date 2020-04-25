67a2ca9cef228e33ca402a332f006942733120e5
kbuchiane <kbuchiane@gmail.com>
2020-04-24 20:03:55 -0400

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <template>
   <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+      <p
+        @userlogin="setUsername()"
+        @usersignup="setUsername()"
+        class="bannerUsername"
+      >{{ userData.username }}</p>
+      <v-btn @click="logout()" color="orange" class="bannerLogoutButton">Log Out</v-btn>
+    </v-app-bar>
+
     <v-content>
       <Index />
     </v-content>
@@ -16,7 +46,31 @@ export default {
     Index
   },
   data: () => ({
-    //
-  })
+    serverUrl: "http://localhost:3000",
+    userData: {
+      username: "username",
+      email: ""
+    }
+  }),
+  methods: {
+    logout: function() {
+      this.serverLogout(this.userData.username).then(data => {
+        this.userData.username = "";
+      });
+    },
+    serverLogout: function(username) {
+      return axios
+        .get(this.serverUrl + "/logout", {
+          params: {
+            username: username
+          }
+        })
+        .then(function(response) {
+          return response.data;
+        });
+    }
+  }
 };
 </script>
+
+<style scoped src='./assets/styles/app.css'></style>
