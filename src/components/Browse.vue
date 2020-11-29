311fe9ca064e324a37aa752d65c4ec457a0e35e5
kbuchiane <kbuchiane@gmail.com>
2020-11-29 08:13:20 -0500

    browse page progress

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,13 +1,20 @@
 <template>
   <v-container>
+    <div id="clip">
+        <Clip/>
+    </div>
   </v-container>
 </template>
 
 <script>
+import Clip from "./Clip";
 import axios from "axios";
 
 export default {
   name: "Browse",
+  components: {
+    Clip
+  },
   data: () => ({
     //
   })
---
+++ src/components/Clip.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+      <div>
+          This will be a clip!
+      </div>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Clip",
+  data: () => ({
+    //
+  })
+};
+</script>
+
+<style scoped src='../assets/styles/clip.css'></style>
