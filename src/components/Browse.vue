ea3642594618c58a3fa797778fdfbe70847e287a
jnbuchi <jack.buchiane@gmail.com>
2020-12-20 09:41:04 -0500

    inital development of follow game handler

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -6,12 +6,26 @@
         <p></p>
         <v-row justify="center">
           <v-btn
-            @click="follow(followerUsername, followedUsername)"
+            @click="followUser(followerUsername, followedUsername)"
             color="green"
             class="logsignButton"
-          >Test</v-btn>
+            >Test
+          </v-btn>
         </v-row>
         <p></p>
+        <v-row justify="center">
+          <v-btn
+            @click="followGame(followerUsername, followedGame)"
+            color="green"
+            class="logsignButton"
+            >Follow Game
+          </v-btn>
+        </v-row>
+        <p></p>
+        <p></p>
+        <v-row justify="center">
+          <p class="browseMessage noselect">{{ statusMessage }}</p>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -20,39 +34,70 @@
 <script>
 import axios from "axios";
 import appConfig from "../config/app.config";
-import userEntryUtil from "../utils/userEntry.util";
+
 export default {
   name: "Browse",
   data: () => ({
     serverUrl: appConfig.SERVER_URL,
-    followerUsername: "jon",
-    followedUsername: "jackie"
+    // followerUsername: "jon",
+    followedUsername: "jackie",
+    followerUsername: "jack",
+    followedGame: "frogger",
+    statusMessage: ""
   }),
   props: ["user"],
   methods: {
-    follow: function(followerUsername, followedUsername) {
+    followUser: function (followerUsername, followedUsername) {
       followerUsername = followerUsername.trim();
       followedUsername = followedUsername.trim();
 
       return axios({
-        method: 'post',
+        method: "post",
         url: this.serverUrl + "/followUser",
-        headers: { 
-          authorization: "Bearer " + this.user.accessToken
+        headers: {
+          authorization: "Bearer " + this.user.accessToken,
         },
         data: {
           followerUsername: followerUsername,
-          followedUsername: followedUsername
+          followedUsername: followedUsername,
         },
-      })
-      .then(function(response) {
+      }).then(function (response) {
+        this.statusMessage = response.data.message;
         return response;
       });
-    }
+    },
+
+    followGame: function (followerUsername, followedGame) {
+      followerUsername = followerUsername.trim();
+      followedGame = followedGame.trim();
+
+      return axios({
+        method: "post",
+        url: this.serverUrl + "/followGame",
+        headers: {
+          authorization: "Bearer " + this.user.accessToken,
+        },
+        data: {
+          followerUsername: followerUsername,
+          followedGame: followedGame,
+        },
+      })
+        .then(function (response) {
+          this.statusMessage = response.data.message;
+          return response;      
+        });
+    },
   },
-  mounted: function() {
+
+  clearEntries: function () {
+    this.followedUsername = "";
+    this.followedGame = "";
+    this.statusMessage = "";
+  },
+
+  mounted: function () {
     // this.$refs.usernameInput.focus();
-  }
+  },
 };
 </script>
 
