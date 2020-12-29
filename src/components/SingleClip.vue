8a92d4baef7b40822b45b446e0d9dc6558236345
kbuchiane <kbuchiane@gmail.com>
2020-12-29 10:51:27 -0500

    updated filter bar styles

--- src/components/SingleClip.vue
+++ src/components/SingleClip.vue
@@ -12,21 +12,8 @@
         <div class="filter">
             <div class="filterTitle">Filter Comments By</div>
             <div class="filterOptionsSetOne">
-                <div class="selectedFilterOption">Most Popular</div>
-                <div class="filterOption">Followed Only</div>
-                <div class="filterOption">Specific Games</div>
-                <div class="filterOption">Most Impressive</div>
-                <div class="filterOption">Funniest</div>
-                <div class="filterOption">Best Discussion</div>
-            </div>
-            <div class="filterTitle">Timeframe</div>
-            <div class="filterOptionsSetTwo">
-                <div class="selectedFilterOption">Default</div>
-                <div class="filterOption">Past Day</div>
-                <div class="filterOption">Past Week</div>
-                <div class="filterOption">Past Month</div>
-                <div class="filterOption">Past Year</div>
-                <div class="filterOption">All Time</div>
+                <div class="selectedFilterOption">Most Likes</div>
+                <div class="filterOption">Newest</div>
             </div>
         </div>
     <v-row justify="center" class="text-center">
