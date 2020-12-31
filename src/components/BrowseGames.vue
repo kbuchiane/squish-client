32f5d2e02341257352e01d7f3272598228169b25
kbuchiane <kbuchiane@gmail.com>
2020-12-31 13:45:43 -0500

    tags almost done

--- src/components/BrowseGames.vue
+++ src/components/BrowseGames.vue
@@ -21,8 +21,47 @@
       </div>
     </div>
     <v-row justify="center" class="text-center">
-      <v-col class="mb-5 gameColumn" cols="8">
-        <div v-for="game in games" :key="game.id"></div>
+      <v-col class="mb-5 gameColumn" cols="6">
+        <div v-for="game in games" :key="game.id">
+          <div class="gameDiv">
+            <div class="gameTitle">
+              <p class="gameTitleText">
+                <router-link to="/game" class="routerStyle">
+                  {{ game.title }}
+                </router-link>
+              </p>
+            </div>
+            <div class="gameReleaseDate">
+              Release Date: {{ game.releaseDate }}
+            </div>
+            <div class="gameHeader">
+              <div class="gameImageDiv">
+                <router-link to="/game">
+                  <img class="gameImage" contain :src="game.icon" />
+                </router-link>
+              </div>
+              <div class="gameTags">
+                <p v-for="tag in game.tags" :key="tag" class="gameTag">
+                  {{ tag }}
+                </p>
+              </div>
+              <div class="gameUserActions">
+                <v-btn color="#40a0e0" class="userActionButton">Follow</v-btn>
+              </div>
+            </div>
+            <div class="gameInfoDiv">
+              <div class="gameInfoSection">
+                {{ game.followerCount }} followers
+              </div>
+              <div class="gameInfoSection">
+                {{ game.clipsTodayCount }} new clips today
+              </div>
+              <div class="gameInfoSection">
+                {{ game.clipsAllTimeCount }} clips all time
+              </div>
+            </div>
+          </div>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -40,12 +79,22 @@ export default {
       {
         id: "1",
         title: "PLAYERUNKNOWN's BATTLEGROUNDS",
-        icon: require("../assets/images/pubg.png")
+        icon: require("../assets/images/pubg.png"),
+        releaseDate: "Dec 2017",
+        followerCount: "84k",
+        clipsTodayCount: "32k",
+        clipsAllTimeCount: "456k",
+        tags: ["Battle Royale", "Shooter"],
       },
       {
         id: "2",
-        title: "PLAYERUNKNOWN's BATTLEGROUNDS",
-        icon: require("../assets/images/pubg.png")
+        title: "Astrofire",
+        icon: require("../assets/images/astrofire.png"),
+        releaseDate: "1994",
+        followerCount: "23",
+        clipsTodayCount: "1",
+        clipsAllTimeCount: "14",
+        tags: ["Classic"],
       },
     ],
   }),
