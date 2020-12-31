9337660a5985e582a61b3a07bda604dba1cc6316
kbuchiane <kbuchiane@gmail.com>
2020-12-31 14:44:52 -0500

    page looks good

--- src/components/BrowseGames.vue
+++ src/components/BrowseGames.vue
@@ -3,21 +3,9 @@
     <div class="filter">
       <div class="filterTitle">Filter Games By</div>
       <div class="filterOptionsSetOne">
-        <div class="selectedFilterOption">Most Popular</div>
-        <div class="filterOption">Followed Only</div>
-        <div class="filterOption">Specific Games</div>
-        <div class="filterOption">Most Impressive</div>
-        <div class="filterOption">Funniest</div>
-        <div class="filterOption">Best Discussion</div>
-      </div>
-      <div class="filterTitle">Timeframe</div>
-      <div class="filterOptionsSetTwo">
-        <div class="selectedFilterOption">Default</div>
-        <div class="filterOption">Past Day</div>
-        <div class="filterOption">Past Week</div>
-        <div class="filterOption">Past Month</div>
-        <div class="filterOption">Past Year</div>
-        <div class="filterOption">All Time</div>
+        <div class="selectedFilterOption">Most Followed</div>
+        <div class="filterOption">Most Clips Today</div>
+        <div class="filterOption">Specific Tags</div>
       </div>
     </div>
     <v-row justify="center" class="text-center">
