83d7acfe40c74a58a34e0cf76758902f229c6f37
kbuchiane <kbuchiane@gmail.com>
2021-01-03 18:54:50 -0500

    post page progress

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,8 +3,32 @@
     <v-row justify="center" class="text-center">
       <v-col class="mb-5 postColumn" cols="6">
         <div class="postDiv">
-          <div class="browseClipDiv">Browse Filesystem for Clip</div>
-          <div class="browsePosterDiv">Browse Filesystem for Poster</div>
+          <div class="postHeader">Post A New Clip</div>
+          <div class="browseClipDiv">
+            <v-btn @click="uploadClip()" color="#40a0e0" class="postButton"
+              >Upload Clip</v-btn
+            >
+            <input
+              type="file"
+              class="uploadInput"
+              ref="clipInput"
+              accept="*.mp4"
+              @change="clipSelected"
+            />
+            <p class="uploadClipFilename">somename.mp4 {{ newClipName }}</p>
+          </div>
+          <div class="browsePosterDiv">
+            <v-btn @click="uploadThumbnail()" color="#40a0e0" class="postButton"
+              >Upload Thumbnail</v-btn
+            >
+            <input
+              type="file"
+              class="uploadInput"
+              ref="thumbnailInput"
+              accept="image/*"
+              @change="thumbnailSelected"
+            />
+          </div>
           <div class="clipTitleDiv">Clip Title</div>
           <div class="gameTitleDiv">Clip Game</div>
         </div>
@@ -23,10 +47,28 @@ export default {
   data: () => ({
     serverUrl: appConfig.SERVER_URL,
     newClip: "",
+    newClipName: "",
     newClipPoster: "",
     newClipTitle: "",
     newClipGame: "",
   }),
+  methods: {
+    uploadClip: function () {
+      console.log("uploading a clip");
+      this.$refs.clipInput.click();
+    },
+    clipSelected: function(event) {
+      console.log('a clip was selected');
+      let files = event.target.files;
+      this.newClipName = files[0].name;
+      let fileReader = new FileReader();
+      fileReader.addEventListener("load", () => {
+        this.clipUrl = fileReader.result;
+      });
+      fileReader.readAsDataURL(files[0]);
+      this.newClip = files[0];
+    }
+  },
 };
 </script>
 
